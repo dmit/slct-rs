4fc93473aa53cec323c8e14e42e6d39e05140e7d
Dmitri Melnikov <github@dmit.me>
2014-09-19 21:26:40 +0300

    Word and cluster thresholds can now be passed as flags

--- slct.rs
+++ slct.rs
@@ -1,16 +1,62 @@
+extern crate getopts;
+
 use std::collections::HashMap;
 use std::io::{BufferedReader, File};
 use std::os;
+use std::rt::heap;
 
-static WORD_THRESHOLD: u32 = 1000;
-static CLUSTER_THRESHOLD: u32 = 1000;
+use getopts::{getopts,optflag,optopt};
+
+type Hash = uint;
+type Count = u32;
+type Words = HashMap<Hash, Count>;
+type Clusters = HashMap<String, Count>;
 
 fn main() {
-    let args = os::args();
-    let inputs: Vec<Path> = args.tail().iter().map(|f| Path::new(f.as_slice())).collect();
+    let opts = [
+        optopt("c", "cluster-threshold", "cluster appearance threshold for diplay", "CLUSTERTHRESHOLD"),
+        optflag("m", "mem-stats", "print memory allocator statistics at the end"),
+        optflag("r", "rare", "display only clusters below CLUSTERTHRESHOLD"),
+        optopt("w", "word-threshold", "minimum frequency of a word to be considered for a cluster", "WORDTHRESHOLD"),
+    ];
+    let matches = getopts(os::args().tail(), opts).unwrap_or_else(|f| fail!(f.to_string()));
 
-    let mut word_freq: HashMap<u64, u32> = HashMap::new();
-    for path in inputs.iter() {
+    let cluster_threshold: Count = match matches.opt_str("c") {
+        Some(ct) => { from_str(ct.as_slice()).unwrap() }
+        None => { 1000 }
+    };
+
+    let word_threshold: Count = match matches.opt_str("w") {
+        Some(wt) => { from_str(wt.as_slice()).unwrap() }
+        None => { 1000 }
+    };
+
+    let show_rare = matches.opt_present("r");
+    let print_mem_stats = matches.opt_present("m");
+
+    let inputs: Vec<Path> = matches.free.iter().map(|f| Path::new(f.as_slice())).collect();
+
+    let word_freq = calc_word_freq(&inputs);
+    println!("found {} unique words", word_freq.len());
+
+    let clusters = calc_clusters(&inputs, &word_freq, word_threshold);
+    println!("found {} clusters", clusters.len());
+
+    if print_mem_stats {
+        heap::stats_print();
+    }
+
+    for (k,v) in clusters.iter() {
+        if show_rare == *v < cluster_threshold {
+            println!("{}\t{}", v, k);
+        }
+    }
+}
+
+fn calc_word_freq(paths: &Vec<Path>) -> Words {
+    let mut word_freq: HashMap<Hash, Count> = HashMap::new();
+
+    for path in paths.iter() {
         let mut file = BufferedReader::new(File::open(path));
         for l in file.lines() {
             for w in l.ok().unwrap().as_slice().words() {
@@ -20,31 +66,28 @@ fn main() {
         }
     }
 
-    println!("words found: {}", word_freq.len());
+    word_freq
+}
 
-    let mut clusters: HashMap<String, u32> = HashMap::new();
-    for path in inputs.iter() {
+fn calc_clusters(paths: &Vec<Path>, word_freq: &Words, word_threshold: Count) -> Clusters {
+    let mut clusters: HashMap<String, Count> = HashMap::new();
+
+    for path in paths.iter() {
         let mut file = BufferedReader::new(File::open(path));
         for l in file.lines() {
-            let cluster = clusterify(&word_freq, l.ok().unwrap().as_slice());
+            let cluster = clusterify(l.unwrap().as_slice(), word_freq, word_threshold);
             if !cluster.is_empty() {
                 clusters.insert_or_update_with(cluster, 1, |_k,v| *v+=1);
             }
         }
     }
 
-    println!("clusters found: {}", clusters.len());
-
-    for (k,v) in clusters.iter() {
-        if *v >= CLUSTER_THRESHOLD {
-            println!("{}\t{}", v, k);
-        }
-    }
+    clusters
 }
 
-fn clusterify(word_freq: &HashMap<u64, u32>, line: &str) -> String {
+fn clusterify(line: &str, word_freq: &Words, word_threshold: Count) -> String {
     let words: Vec<&str> = line.words().map({|w|
-        if word_freq[fnv1a(w)] < WORD_THRESHOLD {
+        if word_freq[fnv1a(w)] < word_threshold {
             "*"
         } else {
             w
@@ -54,13 +97,13 @@ fn clusterify(word_freq: &HashMap<u64, u32>, line: &str) -> String {
     words.connect(" ")
 }
 
-static FNV_PRIME_64: u64 = 1099511628211;
-static FNV1_OFFSET_BASIS_64: u64 = 14695981039346656037;
+static FNV_PRIME_64: Hash = 1099511628211;
+static FNV1_OFFSET_BASIS_64: Hash = 14695981039346656037;
 
-fn fnv1a(s: &str) -> u64 {
+fn fnv1a(s: &str) -> Hash {
     let mut hash = FNV1_OFFSET_BASIS_64;
     for b in s.as_bytes().iter() {
-        hash = (hash ^ *b as u64) * FNV_PRIME_64;
+        hash = (hash ^ *b as Hash) * FNV_PRIME_64;
     }
     hash
 }
